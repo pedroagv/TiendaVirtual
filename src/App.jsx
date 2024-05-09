eac86809c0b37a852dc7cd1d1c0e3c6b175feef2
pedroagv <escomoloquiera@gmail.com>
2024-05-09 17:48:28 -0500

    primer subida

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Contacto from "./components/Contacto";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
+import Navbar from "./components/Navbar";
+import Nosotros from "./components/Nosotros";
+import "./main.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+import Carrito from "./components/Carrito";
+import Checkout from "./components/Checkout";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <CartProvider>
+        <BrowserRouter>
+
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/item/:id" element={<ItemDetailContainer />}/>
+            <Route path="/productos" element={<ItemListContainer />} />
+            <Route path="/productos/:categoria" element={<ItemListContainer />} />
+            <Route path="/nosotros" element={<Nosotros />}/>
+            <Route path="/contacto" element={<Contacto />}/>
+            <Route path="/carrito" element={<Carrito />}/>
+            <Route path="/checkout" element={<Checkout />}/>
+          </Routes>
+          
+          <Footer />
+        </BrowserRouter>
+      </CartProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>Números de teléfono: 123-456-789 / 987-654-321</p>
+        </div>
+    )
+}
+
+export default Footer
